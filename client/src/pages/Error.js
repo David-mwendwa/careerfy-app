7920c710b754372d03e88c16da8a1d6f758107d5
techdave <klatzdavie19@gmail.com>
2022-03-12 23:10:19 +0300

    complete errror page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Ohh! page not found</h3>
+        <p>We can't find the page you're looking for</p>
+        <Link to='/'>back home</Link>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Error
+export default Error;
