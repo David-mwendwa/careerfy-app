d480ad8b103605cbb16787f36409964cc4254afd
techdave <klatzdavie19@gmail.com>
2022-03-13 08:46:30 +0300

    add alert component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Logo, FormRow } from '../components';
+import { Logo, FormRow, Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
@@ -7,6 +7,7 @@ const initialState = {
   email: '',
   password: '',
   isMember: true,
+  showAlert: true,
 };
 
 const Register = () => {
@@ -24,6 +25,7 @@ const Register = () => {
       <form className='form' onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
+        {values.showAlert && <Alert/>}
         <FormRow
           text='text'
           value={values.name}
