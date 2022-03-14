a7f59015977ff19df29e8c814b3e3b947109be08
techdave <klatzdavie19@gmail.com>
2022-03-14 17:11:12 +0300

    create reducer

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,4 +1,6 @@
-import React, { useState, useReducer, useContext } from 'react';
+import React, { useReducer, useContext } from 'react';
+
+import reducer from './reducer';
 
 const initialState = {
   isLoading: false,
@@ -10,7 +12,7 @@ const initialState = {
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [state, setState] = useState(initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
