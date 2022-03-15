e90f83bf3e133e68a38ac0d07bfb06cd45967c15
techdave <klatzdavie19@gmail.com>
2022-03-15 14:22:04 +0300

    set up basic express server

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome')
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server listening on port ${port}`))
