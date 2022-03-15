19331824c7e5a7f04da23844fc82c36b548833aa
techdave <klatzdavie19@gmail.com>
2022-03-15 18:53:37 +0300

    check for duplicate email in controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,30 +1,15 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.NOT_FOUND;
-  }
-}
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     throw new BadRequestError('please provide all values');
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
   }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ status: 'success', user });
