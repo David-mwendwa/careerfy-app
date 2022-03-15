96041c615f69010b56f4ec97c3d5fe6c35a92fa8
techdave <klatzdavie19@gmail.com>
2022-03-15 17:02:55 +0300

    pass error to error middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,12 @@
 import User from '../models/User.js';
 import errorHandlerMiddleware from '../middleware/error-handler.js';
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({status: 'success', user });
+    res.status(201).json({ status: 'success', user });
   } catch (error) {
-    res.status(500).json({ msg: 'There was an error' });
+    next(error);
   }
 };
 
