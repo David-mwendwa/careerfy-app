cabaaffd424da9a4862863efb3b4839093a013c2
techdave <klatzdavie19@gmail.com>
2022-03-15 18:11:36 +0300

    handle unique field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -5,11 +5,16 @@ const errorHandlerMiddleware = async (err, req, res, next) => {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: 'Something went wrong, please try again later',
   };
+  // handle missing field error
   if (err.name === 'ValidationError') {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
     defaultError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(', ');
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
   }
   // res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
