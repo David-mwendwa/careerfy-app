6e6d54c8df185dff3c23bc58cae07c470a0afb9c
techdave <klatzdavie19@gmail.com>
2022-03-15 18:36:26 +0300

    extend badRequestError and NotFoundError handlers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,30 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 
-class CustomAPIError extends Error{
+class CustomAPIError extends Error {
   constructor(message) {
-    super(message)
-    this.statusCode = StatusCodes.BAD_REQUEST
+    super(message);
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new CustomAPIError('please provide all values');
+    throw new BadRequestError('please provide all values');
   }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ status: 'success', user });
