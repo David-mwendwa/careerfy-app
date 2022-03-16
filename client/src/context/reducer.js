b37c66775ef724079d31c15cb71637954d5f0f3b
techdave <klatzdavie19@gmail.com>
2022-03-16 14:04:28 +0300

    complete user login on frontend

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -51,6 +51,32 @@ const reducer = (state, action) => {
       alertText: action.payload.msg,
     };
   }
+
+  if (action.type === LOGIN_USER_BEGIN) {
+    return { ...state, isLoading: true };
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Login Successful! Redirecting...',
+    };
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    };
+  }
   throw new Error(`no such action: ${action.type}`);
 };
 
