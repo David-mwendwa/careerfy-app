83186e5e1c1054026dfbd213aeed14d3fb53b127
techdave <klatzdavie19@gmail.com>
2022-03-17 15:53:25 +0300

    complete navbar structure

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import logo from '../assets/images/logo.svg';
 
 const Logo = () => {
-  return <h1 style={{ color: '#54d1db' }}>careerfy</h1>;
+  return <img src={logo} alt='jobify' className='logo' />;
 };
+
+// const Logo = () => {
+//   return <h1 style={{ color: '#54d1db' }}>careerfy</h1>;
+// };
 
 export default Logo;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
-import {FaHome} from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import Logo from './Logo';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>navbar</h4>
-
+      <div className='nav-center'>
+        <button
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar')}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => console.log('show/hide dropdown')}>
+            <FaUserCircle />
+            david
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}>
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
