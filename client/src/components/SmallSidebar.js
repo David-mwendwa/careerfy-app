3af04bbae14e13f86fbc4967d1c3623463bbaa3d
techdave <klatzdavie19@gmail.com>
2022-03-17 16:59:35 +0300

    setup small sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/SmallSidebar'
+import React from 'react';
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h4>small sidebar</h4>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button
+            type='button'
+            className='close-btn'
+            onClick={() => console.log('toggle sidebar')}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+            <div className="nav-links">
+              nav links
+            </div>
+          </header>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
