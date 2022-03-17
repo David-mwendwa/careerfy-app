5ccf6ba1632bcb4c173f64a25f31df19cd88b097
techdave <klatzdavie19@gmail.com>
2022-03-17 17:06:37 +0300

    add toggle functionality for small sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -7,21 +7,20 @@ import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div className='sidebar-container show-sidebar'>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }>
         <div className='content'>
-          <button
-            type='button'
-            className='close-btn'
-            onClick={() => console.log('toggle sidebar')}>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
             <FaTimes />
           </button>
           <header>
             <Logo />
-            <div className="nav-links">
-              nav links
-            </div>
+            <div className='nav-links'>nav links</div>
           </header>
         </div>
       </div>
