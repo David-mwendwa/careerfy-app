df05bf8dd93d181dfefb4736ebf6f0e1c76d0aba
techdave <klatzdavie19@gmail.com>
2022-03-17 12:29:47 +0300

    add protectedRoute component - only for loggedin users

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Error, Landing, Register } from './pages';
+import { Error, Landing, Register, ProtectedRoute } from './pages';
 import {
   AddJob,
   AllJobs,
@@ -12,7 +12,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SharedLayout/>}>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }>
           <Route index element={<Stats />} />
           <Route path='/all-jobs' element={<AllJobs />} />
           <Route path='/add-job' element={<AddJob />} />
