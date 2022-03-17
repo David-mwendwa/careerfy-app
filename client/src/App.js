b28d8a44853eeb80a1219ff86bcde5a14d92a6d3
techdave <klatzdavie19@gmail.com>
2022-03-17 11:56:57 +0300

    set up nested dashboard pages

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>dashboard</div>} />
+        <Route path='/'>
+          <Route path='/stats' element={<Stats />} />
+          <Route path='/all-jobs' element={<AllJobs />} />
+          <Route path='/add-job' element={<AddJob />} />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
