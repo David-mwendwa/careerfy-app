8226bf0627a421614945e95fcba48769cbcc5c8d
techdave <klatzdavie19@gmail.com>
2022-03-18 12:43:44 +0300

    create bigSidebar component

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/BigSidebar'
+import React from 'react';
+import Wrapper from '../assets/wrappers/BigSidebar';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
+import { useAppContext } from '../context/appContext';
 
 const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>big sidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }>
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default BigSidebar
+export default BigSidebar;
