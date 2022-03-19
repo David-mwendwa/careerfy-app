6a6bee23de5c8445dfc15b23be4bcb484e65cfeb
techdave <klatzdavie19@gmail.com>
2022-03-19 13:14:26 +0300

    create _updateUser_ controller

--- models/User.js
+++ models/User.js
@@ -42,8 +42,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function () {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  // const salt = await bcrypt.genSalt(10);
+  // this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.createJWT = function () {
