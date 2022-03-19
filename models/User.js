d37c38ab6460d01799c4c11e4858dbb10bc50ab1
techdave <klatzdavie19@gmail.com>
2022-03-19 13:26:04 +0300

    check for password modification using 'modifiedPaths()'

--- models/User.js
+++ models/User.js
@@ -42,8 +42,9 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function () {
-  // const salt = await bcrypt.genSalt(10);
-  // this.password = await bcrypt.hash(this.password, salt);
+  if (!this.modifiedPaths('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.createJWT = function () {
