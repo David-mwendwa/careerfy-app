a66ce9056c645c0cbecea14275ead27dbcc5406a
techdave <klatzdavie19@gmail.com>
2022-03-22 21:42:38 +0300

    enhance user logout - frontend

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -63,7 +63,7 @@ const AppProvider = ({ children }) => {
     (error) => {
       console.log(error.response);
       if (error.response.status === 401) {
-        console.log('AUTH ERROR');
+        logoutUser()
       }
       return Promise.reject(error);
     }
@@ -152,7 +152,12 @@ const AppProvider = ({ children }) => {
       });
       addUserToLocalStorage({ user, location, token });
     } catch (error) {
-      dispatch({ type: UPDATE_USER_ERROR, payload: { msg: error.response.data.msg } })
+      if (error.response.status !== 401) {
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: error.response.data.msg },
+        });
+      }
     }
     clearAlert()
   };
