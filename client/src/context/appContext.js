b3cb9ae5ca66ef02d752c69f5ba922c56dfafbe1
techdave <klatzdavie19@gmail.com>
2022-03-22 19:57:24 +0300

    set up axios interceptors

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -39,10 +39,32 @@ const AppProvider = ({ children }) => {
   // axios
   const authFetch = axios.create({
     baseURL: '/api/v1',
-    headers: {
-      Authorization: `Bearer ${state.token}`,
-    },
   });
+
+  // request interceptor
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  // response interceptor
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  );
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
