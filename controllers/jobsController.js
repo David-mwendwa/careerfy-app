3dab558b3a2f0274ff3bb311e3d64e5ce58068b0
techdave <klatzdavie19@gmail.com>
2022-03-23 08:31:04 +0300

    create _createJob_ controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,9 +1,19 @@
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+
 const getAllJobs = async (req, res) => {
   res.send('getAllJobs');
 };
 
 const createJob = async (req, res) => {
-  res.send('createJob');
+  const { position, company } = req.body;
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values');
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
