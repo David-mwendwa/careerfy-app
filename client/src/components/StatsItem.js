13bee52dfcc3138c32e5e24af0067417dc569381
techdave <klatzdavie19@gmail.com>
2022-03-27 14:03:53 +0300

    build stats item component

--- client/src/components/StatsItem.js
+++ client/src/components/StatsItem.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Wrapper from '../assets/wrappers/StatItem';
 
-const StatsItem = () => {
+const StatsItem = ({ count, title, icon, color, bcg }) => {
   return (
-    <div>StatsItem</div>
-  )
-}
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <div className='icon'>{icon}</div>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
+  );
+};
 
-export default StatsItem
+export default StatsItem;
