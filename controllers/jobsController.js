4bf25b8ac3c0f6cee0c7f287f95713a23e0682a4
techdave <klatzdavie19@gmail.com>
2022-03-27 08:46:03 +0300

    reduce stats

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -63,6 +63,13 @@ const showStats = async (req, res) => {
     { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
     { $group: { _id: '$status', count: { $sum: 1 } } },
   ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+  
   res.status(StatusCodes.OK).json({ stats });
 };
 
