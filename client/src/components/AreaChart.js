7b161a7a5d9940f64191b83a2259c51b420321dc
techdave <klatzdavie19@gmail.com>
2022-03-28 08:17:29 +0300

    create _Area Chart_ component

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid,
+} from 'recharts';
 
-const AreaChart = () => {
+const AreaChartComponent = ({ data }) => {
   return (
-    <div>
-      <h2>Area Chart</h2>
-    </div>
-  )
-}
+    <ResponsiveContainer width='100%' height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis datakey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd' />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default AreaChart
+export default AreaChartComponent;
