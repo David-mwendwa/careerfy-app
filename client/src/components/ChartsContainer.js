9938d84216fe2ba2eb9ee7bef18b38606a5bb960
techdave <klatzdavie19@gmail.com>
2022-03-28 07:46:32 +0300

    build _ChartsContainer_ component - frontend

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/ChartsContainer';
 
 const ChartContainer = () => {
-  return (
-    <div>ChartContainer</div>
-  )
-}
+  const [barChart, setBarChart] = useState(true);
+  const { montlyApplications: data } = useAppContext();
 
-export default ChartContainer
+  return (
+    <Wrapper>
+      <h4>Montly Applications</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'AreaChart' : 'BarChart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
+};
+
+export default ChartContainer;
