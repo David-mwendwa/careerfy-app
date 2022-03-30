c8202e78eb76fa9aae3b326f6d2e8706921c9d48
techdave <klatzdavie19@gmail.com>
2022-03-30 08:50:07 +0300

    implement list of buttons for the pagination

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -6,6 +6,11 @@ import Wrapper from '../assets/wrappers/PageBtnContainer';
 const PageBtnContainer = () => {
   const { numOfPages, page } = useAppContext();
 
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+  console.log(pages);
+
   const prevPage = () => {
     console.log('prev page');
   };
@@ -18,7 +23,19 @@ const PageBtnContainer = () => {
         <HiChevronDoubleLeft />
         prev
       </button>
-      <div className='btn-container'>buttons</div>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type='button'
+              className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
+              key={pageNumber}
+              onClick={() => console.log('change page')}>
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
       <button className='next-btn' onClick={nextPage}>
         next <HiChevronDoubleRight />
       </button>
