1351471d881a23090414cb1fe6e1b03523ec3a40
techdave <klatzdavie19@gmail.com>
2022-03-30 08:08:33 +0300

    setup pagination - page & limit

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
 import mongoose from 'mongoose';
-import moment from 'moment'; 
+import moment from 'moment';
 
 const getAllJobs = async (req, res) => {
   const { status, jobType, sort, search } = req.query;
@@ -38,6 +38,12 @@ const getAllJobs = async (req, res) => {
   if (sort === 'z-a') {
     result = result.sort('-position');
   }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const jobs = await result;
 
