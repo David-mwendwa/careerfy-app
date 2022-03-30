096462d67366f84b7b260ca944754991b6147b47
techdave <klatzdavie19@gmail.com>
2022-03-30 07:29:59 +0300

    complete search component

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -18,10 +18,15 @@ const SearchContainer = () => {
   } = useAppContext();
 
   const handleSearch = (e) => {
-    if (isLoading) return
-    handleChange({name:e.target.name, value: e.target.value})
+    if (isLoading) return;
+    handleChange({ name: e.target.name, value: e.target.value });
     console.log(e.taget.name);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    clearFilters()
+  }
 
   return (
     <Wrapper>
@@ -32,8 +37,31 @@ const SearchContainer = () => {
             type='text'
             name='search'
             value={search}
-            handleChange={handleChange}
+            handleChange={handleSearch}
           />
+          <FormRowSelect
+            labelText='status'
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+          <FormRowSelect
+            labelText='type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button className='btn btn-block btn-danger' disabled={isLoading} onClick={handleSubmit}>
+            clear filters
+          </button>
         </div>
       </form>
     </Wrapper>
